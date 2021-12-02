ddd75d3107f8cc50e3b711fb96c1f8d01106ba6a
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-12-02 18:34:27 -0500

    supportingFunctions.R
    I think the second task is mostly done but I can't test it right now

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -25,11 +25,14 @@ toCSV <- function(dir){
 
 # 2. compile data from all .csv files in a directory into a single .csv file
   # file should have original 12 columns, adding "country" and "dayofYear"
-  # user should choose to remove/keep rows with NA's (+/- warning)
+  # user can choose to remove/keep rows with NA's (+/- warning)
 
-oneCSV <- function(path){
+oneCSV <- function(dir1, dir2){
+  #define directories -- this will be different for each input of data
+  directory1 <- "/Users/alexdosch/Desktop/countryX"
+  directory2 <- "/Users/alexdosch/Desktop/countryY_csv"
   # define set of files
-  files <- list.files(path, pattern = ".csv")
+  files <- list.files(directory1, pattern = ".csv")
   # open each file
   for (i in files){
   read.csv("files[i]", header = TRUE, stringsAsFactors = FALSE) ### this has invalid description argument error and idk why
@@ -37,28 +40,37 @@ oneCSV <- function(path){
   with_columns <- file[i]$country <- c("X")
   with_columns <- file[i]$dayofYear <- c(120:175)
   }
+  
+  # same thing but for directory 2 (try to make this a for loop if possible)
+  files <- list.files(directory2, pattern = ".csv")
+  # open each file
+  for (i in files){
+  read.csv("files[i]", header = TRUE, stringsAsFactors = FALSE) ### this has invalid description argument error and idk why
+  # add columns "country" and "dayofYear"
+  with_columns <- file[i]$country <- c("Y")
+  with_columns <- file[i]$dayofYear <- c(120:175)
+  } 
+  
   # append/combine the new files
   allData <- lapply(with_columns, function (x) read.csv(file=x, header=TRUE))
   Reduce(function(x,y) merge(x,y), allData)
-  
-  # handle NA's [argument] --> user interaction ###Should this go at the top?
+ 
+# handle NA's [argument] --> user interaction
   print("Data may contain NA's")
   answer <-readline("If you would like to remove NA's reply REMOVE. If you would like to keep NA's reply KEEP")
-  if (answer!="REMOVE") {
-    #code to remove NA's 
+  if (answer!="REMOVE") { 
     na.omit(allData)
   } else if (answer!="KEEP") {
     answer <- readline("Print warning if NA's are found? Y or N")
     if (answer!="Y"){
-      # code to find NA's in data and print warning
-      if (is.na(allData)=TRUE){
+      (is.na(allData)=TRUE)
         print("Warning: missing values")
       }
     } else {
     break
     }
-  }
-}
+}        
+
 
 
 # summarize compiled data in terms of number of screens run, percent of infected 
