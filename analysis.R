de9c112b9735abc7ed07a9f088dddc385eb470e5
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-12-02 23:17:01 -0500

    Hopefully something close to final edits

--- analysis.R
+++ analysis.R
@@ -1,35 +1,94 @@
 # load functions in supportingFunctions.R, compile and process data, and answer
-  #questions with graphical evidence
+#questions with graphical evidence (Ansers are at the bottom!)
 
 # source() to access supportingFunctions.R
 source("supportingFunctions.R", local = TRUE)
-# use second function to compile the data, input path to a certain directory 
-### change input to be two directories in supportingFunctions.R and mash them all up into one  
-## ideally (if i can fix the other code)
-oneCSV("countryX", "countryY_csv")
-# what i might have to resort to
-oneCSV("/Users/alexdosch/Desktop/Rproject2021/countryX")
-oneCSV("/Users/alexdosch/Desktop/Rproject2021/countryY_csv")
+# use functions to compile and process data
+toCSV(Rproject2021)
+oneCSV(countryX, countryY_csv)
+seeData(allData.csv)
 
-# plot 1: double bar graph, color coded by country with y-axis as markers present?
-    # cowplot to show all the data from function 3 actually
-library(ggplot)
+library(ggplot2)
 library(cowplot)
 
-ggplot( #the last few lectures have really helpful examples of syntax)
-  
-# plot 2: line graph with people infected by day color coded by country
-  
-# activate cowplot to show the graphs side by side 
-  plot_grid(plot1, plot2,
-          labels = c("a", "b"),
+# plots: 
+# scatter plot with people infected by day
+#make data.frame with people infected vs day of year?
+
+Q1 <- ggplot(data = allData, 
+             aes(x = dayofYear, y = ___)) +
+  geom_point(color = as.factor(country)) +
+  xlab("Day of Year") +
+  ylab("Number of People Infected") +
+  theme_classic()
+
+
+plot1 <- ggplot(data = allData, aes( x = country, y=marker01)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot2 <- ggplot(data = allData, aes( x = country, y=marker02)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot3 <- ggplot(data = allData, aes( x = country, y=marker03)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot4 <- ggplot(data = allData, aes( x = country, y=marker04)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot5 <- ggplot(data = allData, aes( x = country, y=marker05)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot6 <- ggplot(data = allData, aes( x = country, y=marker06)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot7 <- ggplot(data = allData, aes( x = country, y=marker07)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot8 <- ggplot(data = allData, aes( x = country, y=marker08)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot9 <- ggplot(data = allData, aes( x = country, y=marker09)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+plot10 <- ggplot(data = allData, aes( x = country, y=marker10)) +
+  geom_col() +
+  theme_bw() +
+  xlab("Country") +
+  ylab("Marker Present")
+
+Q2 <- plot_grid(plot1, plot2, plot3, plot4, plot5,
+          labels = c("01", "02", "03", "04","05","06","07","08","09","10"),
           rel_widths = c(1, 1),
-          ncol = 2,
-          nrow = 1)
+          ncol = 5,
+          nrow = 2)
 
-# Answers to questions
-  # 1. whichever country with more markers and more people infected probably 
-  # had it first because it's had more time to progress and develop
 
-  # 2. if the markers present are not the same in both countries, no bc the 
-  # presence of different markers indicate morphing of the disease
+
+# Answers:
+ # 1. The country with more markers present and thus more people infected most
+      # likely had the disease first because it has had more time to progress 
+      # and develop than the other country. This is visualized in the plot of 
+      # people infected per day in each country (saved as Q1), showing that 
+      # country X most likely had the disease first.
+  # 2. Since the markers present vary widely between the two countries' data, it 
+      # can be inferred that the disease has mutated too much for a vaccine in 
+      # country Y to be effective in country X. This is visualized by Q1, which 
+      # shows how many of each marker are present in each country. 
